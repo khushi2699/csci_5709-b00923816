7f9e6065f5f362a853fa61162a0536327a58654b
Khushi Shah <67200869+khushi2699@users.noreply.github.com>
2023-02-01 00:08:04 -0400

    Initial project commit
    Initial project commit

---
+++ tutorial-2/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          It is working
+        </p>
+        <p>
+           Tutorial -2: Individual Assignment
+        </p>
+        <p>
+           This is a web application created by Khushi Hirenbhai Shah - B00923816
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
